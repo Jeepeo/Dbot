7d099765fa1e658a3caac3c4b25f0f4536c8a00a
Raphiel Rollerscaperers <raphielscape@outlook.com>
2019-02-10 14:17:19 +0700

    [FIXUP]: modules: admin: use AttributeError as ExceptionHandler
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

--- userbot/modules/admin.py
+++ userbot/modules/admin.py
@@ -103,15 +103,30 @@ async def thanos(e):
             send_inline=True,
             embed_links=True,
         )
+<<<<<<< HEAD
         if (await e.get_reply_message()).sender_id in BRAIN_CHECKER:
             await e.edit("`Ban Error! I am not supposed to ban this user`")
             return
         await e.edit("`Taking last photo of this bitch!Wow! going to kill!`")
+=======
+
+        sender = await bon.get_reply_message()
+        try:
+            if sender.sender_id in BRAIN_CHECKER:
+                await bon.edit("`Ban Error! I am not supposed to ban this user`")
+                return
+        except AttributeError:
+            await bon.edit("`You don't seems to do this right`")
+            return
+
+        await bon.edit("`Whacking the pest!`")
+>>>>>>> 6239efc... [FIXUP]: modules: admin: use AttributeError as ExceptionHandler
         time.sleep(5)
         try:
             await bot(
                 EditBannedRequest(
-                    bon.chat_id, (await bon.get_reply_message()).sender_id,
+                    bon.chat_id,
+                    sender.sender_id,
                     rights
                 )
             )
